36fb085cd432c980819d40a69d88ac2c9d9eaa81
Raymond An <raymondan100@gmail.com>
2019-06-12 15:12:18 -0700

    rocket effects

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour
-{
-    [SerializeField] float rotThrust  = 85f; // for the amount of rotation
+public class Rocket : MonoBehaviour {
+    [SerializeField] float rotThrust = 85f; // for the amount of rotation
     [SerializeField] float mainThrust = 85f; // for the upward thrust
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip levelPass;
+
+    [SerializeField] ParticleSystem mainEngineParticles; // the built in Unity ParticleSystem allows put any particle in the lever and use the .Play to show effect
+    [SerializeField] ParticleSystem explosionParticles;
+    [SerializeField] ParticleSystem levelPassParticles;
+
 
     Rigidbody rigidBody; // declare a variable rigidBody in order to access the RigidBody script
     AudioSource audioSource;
@@ -19,57 +27,36 @@ public class Rocket : MonoBehaviour
 
     }
 
-
     // Update is called once per frame
     void Update() {
-        if (state == State.Alive) {
-            Thrust();
-            Rotate();
+        if (state == State.Alive) { // if rocket hits obstacle, stops any thrusting or user controls but if rocket hasn't hit anything then controls are normal
+            ReactThrustInput();
+            ReactRotatInput();
         }
     }
 
-    void OnCollisionEnter(Collision collision) {
-        if (state != State.Alive) {
-            return;
-        }
-
-        switch (collision.gameObject.tag) { // determines the collisions based on the game tags
-            case "Friendly":
-                break;
-            case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextLevel", 1f); // load the next scene after one second
-                break;
-
-            default:
-                state = State.Dying;
-                Invoke("LoadFirstLevel", 1f);
-                break;
-        }
-    }
-
-    private void LoadNextLevel() {
-        SceneManager.LoadScene(1);
-    }
-
-    private void LoadFirstLevel() {
-        SceneManager.LoadScene(0);
-    }
-
-
-    private void Thrust() {
+    private void ReactThrustInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust); // up is relative to the y arow because it's facing up
-            if (!audioSource.isPlaying) {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else {
             audioSource.Stop();
+            mainEngineParticles.Stop(); // once user stops pressing space, music stops and thrusting particles stop
         }
+
     }
 
-    private void Rotate() {
+    private void ApplyThrust() {
+
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust); // deals with the upward thrust of rocket and audio
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play(); // plays thrusting particles
+
+    }
+
+    private void ReactRotatInput() {
         rigidBody.freezeRotation = true; // this is before we control the rotation of rocket
         float rotationFrame = rotThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A)) {
@@ -82,4 +69,46 @@ public class Rocket : MonoBehaviour
 
         rigidBody.freezeRotation = false; // physics is back
     }
+
+    void OnCollisionEnter(Collision collision) { // deals with what happens when rocket collide with objects
+        if (state != State.Alive) {
+            return;
+        }
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                break;
+            case "Finish": // if rocket hits the platform with the tag "Finish"
+                SuccessSequence();
+                break;
+
+            default: // if the rocket hits any objects that have tags that are untagged
+                DeathSequence();
+                break;
+        }
+    }
+
+    private void SuccessSequence() {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(levelPass);
+        levelPassParticles.Play();
+        Invoke("LoadNextLevel", 1.5f); // load the next scene after one second
+    }
+
+    private void DeathSequence() {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        explosionParticles.Play();
+        Invoke("LoadFirstLevel", 1.5f);
+    }
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene(1); // this is how to switch to next level
+    }
+
+    private void LoadFirstLevel() { // this is to load first level again if you die
+        SceneManager.LoadScene(0);
+    }
+
 }
